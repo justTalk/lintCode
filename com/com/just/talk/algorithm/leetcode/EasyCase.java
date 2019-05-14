28393364b869130ad4055d15e7de28d12011f149
liumingming <mingming.liu@quvideo.com>
2019-05-14 12:06:01 +0800

    author:mingming.liu@quvideo.com

--- com/com/just/talk/algorithm/leetcode/EasyCase.java
+++ com/com/just/talk/algorithm/leetcode/EasyCase.java
@@ -1,6 +1,9 @@
 package com.just.talk.algorithm.leetcode;
 
+import com.just.talk.algorithm.object.ListNode;
+
 import java.util.HashMap;
+import java.util.Stack;
 
 /**
  * Created by Liu On 2019/5/13
@@ -74,11 +77,15 @@ public class EasyCase {
         int reman = 0;
         for (int i = 0; i < s.length(); i++) {
             if ('I' == s.charAt(i)) {
+                if (shouldRemove > 3) {
+                    reman += shouldRemove;
+                    shouldRemove = 0;
+                }
                 shouldRemove += 1;
                 continue;
             }else if ('V' == s.charAt(i)){
                 if (shouldRemove > 5) {
-                    reman += shouldRemove;
+                    reman += shouldRemove + 5;
                 }else {
                     reman += 5 - shouldRemove;
                 }
@@ -86,12 +93,13 @@ public class EasyCase {
                 if (shouldRemove > 0 && shouldRemove < 10) {
                     reman += 10 - shouldRemove;
                 }else {
-                    shouldRemove += 10;
+                    reman += shouldRemove;
+                    shouldRemove = 10;
                     continue;
                 }
             }else if ('L' == s.charAt(i)){
                 if (shouldRemove > 50) {
-                    reman += shouldRemove;
+                    reman += shouldRemove + 50;
                 }else {
                     reman += 50 - shouldRemove;
                 }
@@ -110,5 +118,131 @@ public class EasyCase {
             shouldRemove = 0;
         }
         return reman + shouldRemove;
+    }
+
+    /**
+     * https://leetcode.com/problems/longest-common-prefix/
+     */
+    public String longestCommonPrefix(String[] strs) {
+        if (strs == null || strs.length == 0) {
+            return "";
+        }
+        int minLength = strs[0].length();
+        for (int i = 1; i < strs.length; i++) {
+            if (strs[i].length() < minLength) {
+                minLength = strs[i].length();
+            }
+        }
+        if (minLength == 0) {
+            return "";
+        }
+        int i = 0;
+        for (;i < minLength; i++) {
+            char first = strs[0].charAt(i);
+            for (int j = 1; j < strs.length; j++) {
+                if (first != strs[j].charAt(i)) {
+                    return strs[0].substring(0, i);
+                }
+            }
+        }
+        return strs[0].substring(0, i);
+    }
+
+    /**
+     * https://leetcode.com/problems/valid-parentheses/
+     */
+    public boolean isValid(String s) {
+        if (s == null || s.length() == 0) {
+            return true;
+        }
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(') {
+                stack.push(')');
+                continue;
+            }
+            if (s.charAt(i) == '[') {
+                stack.push(']');
+                continue;
+            }
+            if (s.charAt(i) == '{') {
+                stack.push('}');
+                continue;
+            }
+            if (stack.empty() || s.charAt(i) != stack.peek()) {
+                return false;
+            }else{
+                stack.pop();
+            }
+        }
+        return stack.empty();
+    }
+
+    /**
+     * https://leetcode.com/problems/merge-two-sorted-lists/
+     */
+    public ListNode mergeTwoSortedLists(ListNode l1, ListNode l2) {
+        if (l1 == null) {
+            return l2;
+        }
+        if (l2 == null) {
+            return l1;
+        }
+        ListNode curNode1 = l1;
+        ListNode curNode2 = l2;
+        ListNode preHead = new ListNode(0);
+        ListNode flowNode = preHead;
+        while (curNode1 != null && curNode2 != null){
+            if (curNode1.val < curNode2.val) {
+                flowNode = flowNode.next = curNode1;
+                curNode1 = curNode1.next;
+            }else {
+                flowNode = (flowNode.next = curNode2);
+                curNode2 = curNode2.next;
+            }
+        }
+        ListNode more;
+        if (curNode1 != null) {
+            more = curNode1;
+        }else {
+            more = curNode2;
+        }
+        while (more != null){
+            flowNode = flowNode.next = more;
+            more = more.next;
+        }
+        return preHead.next;
+    }
+
+    /**
+     * https://leetcode.com/problems/remove-duplicates-from-sorted-array/
+     */
+    public int removeDuplicates(int[] nums) {
+        if (nums == null) {
+            return 0;
+        }
+        int validIndex = 0;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[validIndex] != nums[i]) {
+                nums[++validIndex] = nums[i];
+            }
+        }
+        return validIndex + 1;
+    }
+
+    /**
+     * https://leetcode.com/problems/remove-element/
+     */
+    public int removeElement(int[] nums, int val) {
+        if (nums == null  || nums.length == 0) {
+            return 0;
+        }
+        int validIndex = -1;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != val) {
+                nums[++validIndex] = nums[i];
+            }
+        }
+        return validIndex + 1;
     }
 }
